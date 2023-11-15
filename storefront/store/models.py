7fa33f40585b9d5d7861533105cfd0211cd0e456
kumailalidev <kumailalidev@gmail.com>
2023-11-15 23:10:03 +0500

    P01-03-Choice fields.

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -11,8 +11,40 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
+    # choice values
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
+
+    # choices
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, "Bronze"),
+        (MEMBERSHIP_SILVER, "Silver"),
+        (MEMBERSHIP_GOLD, "Gold"),
+    ]
+
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
+    )
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = "P"
+    PAYMENT_STATUS_COMPLETED = "C"
+    PAYMENT_STATUS_FAILED = "F"
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, "Pending"),
+        (PAYMENT_STATUS_COMPLETED, "Completed"),
+        (PAYMENT_STATUS_FAILED, "Failed"),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING
+    )
