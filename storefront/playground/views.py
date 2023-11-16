18b6a802517f89202e83a0d06ab28ac7a97d6c33
kumailalidev <kumailalidev@gmail.com>
2023-11-16 14:07:56 +0500

    P01-05-Retrieving objects.

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 
 from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.all()  # returns a QuerySet object.
-    count = Product.objects.count()  # does not return a query set
+    # P01-05-05-Retrieving Objects
+
+    # when pk is used instead of id, Django will automatically translate
+    # into the name of primary key field. (can be any field)
+    # try:
+    #     product = Product.objects.get(pk=1)  # returns object not a QuerySet
+    # except ObjectDoesNotExist:
+    #     pass
+
+    product = Product.objects.filter(pk=0).first()  # returns None instead of exception
+
+    product = Product.objects.filter(pk=0).exists()  # returns True if product exists
 
     return render(request, "hello.html", {"name": "Kumail"})
