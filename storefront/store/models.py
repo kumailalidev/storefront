b03c4d78ec323548e4a699ea9c022f54a6fb6a9c
kumailalidev <kumailalidev@gmail.com>
2023-11-17 01:19:44 +0500

    P01-06-Adding data validation.

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Promotion(models.Model):
@@ -26,11 +27,19 @@ class Product(models.Model):
     title = models.CharField(max_length=255)  # varchar 255
     slug = models.SlugField()
     description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)  # 9999.99
-    inventory = models.IntegerField()
+    unit_price = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(1)]
+    )  # 9999.99
+    inventory = models.IntegerField(
+        validators=[
+            MinValueValidator(
+                1, message="Inventory value must be greater than or equal to 1."
+            )
+        ]
+    )
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion)
+    promotions = models.ManyToManyField(Promotion, blank=True)
 
     def __str__(self) -> str:
         return self.title
