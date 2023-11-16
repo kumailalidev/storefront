2d3caaccb19857a34f0df11f37118d2c1822969c
kumailalidev <kumailalidev@gmail.com>
2023-11-17 00:58:10 +0500

    P01-06-Creating custom actions.

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models.aggregates import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
@@ -20,6 +20,7 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = ["clear_inventory"]
     list_display = ["title", "unit_price", "inventory_status", "collection_title"]
     list_editable = ["unit_price"]
     list_filter = ["collection", "last_update", InventoryFilter]
@@ -37,6 +38,19 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory < 10:
             return "Low"
         return "OK"
+
+    # custom action
+    @admin.action(description="Clear inventory")
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(
+            inventory=0
+        )  # set inventory of selected objects to 0
+        # display message on success
+        self.message_user(
+            request,
+            f"{updated_count} products were successfully updated.",
+            messages.SUCCESS,  # message type
+        )
 
 
 @admin.register(models.Customer)
