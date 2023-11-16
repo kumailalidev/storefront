8a7be2ffde12a0fb39d7d8925f0b9df7fd9da65b
kumailalidev <kumailalidev@gmail.com>
2023-11-16 14:23:07 +0500

    P01-05-Filtering objects.

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -14,8 +14,24 @@ def say_hello(request):
     # except ObjectDoesNotExist:
     #     pass
 
-    product = Product.objects.filter(pk=0).first()  # returns None instead of exception
+    # product = Product.objects.filter(pk=0).first()  # returns None instead of exception
+    # product = Product.objects.filter(pk=0).exists()  # returns True if product exists
 
-    product = Product.objects.filter(pk=0).exists()  # returns True if product exists
+    # P01-05-06-Filtering Objects
 
-    return render(request, "hello.html", {"name": "Kumail"})
+    # queryset = Product.objects.filter(unit_price__gt=20)  # gt is a field lookup type
+    # queryset = Product.objects.filter(unit_price__range=(20, 30))
+    # queryset = Product.objects.filter(collection__id__range=(1, 2, 3)) # ERROR
+    # queryset = Product.objects.filter(title__icontains="coffee")
+    # queryset = Product.objects.filter(last_update__year=2021)
+    # queryset = Product.objects.filter(last_update__date=2021)
+    # queryset = Product.objects.filter(description__isnull=True)
+
+    return render(
+        request,
+        "hello.html",
+        {
+            "name": "Kumail",
+            "products": list(queryset),
+        },
+    )
