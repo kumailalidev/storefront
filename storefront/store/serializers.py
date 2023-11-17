9a06eded5391d21875a8f50e654dfe05be30461f
kumailalidev <kumailalidev@gmail.com>
2023-11-17 16:56:29 +0500

    P02-02-Creating custom Serializer fields.

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -1,8 +1,21 @@
+from decimal import Decimal
+
 from rest_framework import serializers
+
+from .models import Product
 
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     # name of field does not have to match
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price = serializers.DecimalField(
+        max_digits=6, decimal_places=2, source="unit_price"
+    )
+    # custom field
+    unit_price_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(
+            1.1
+        )  # Decimal and float multiplication not allowed
