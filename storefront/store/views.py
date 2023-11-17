fa8e28305b556ae61e0170eed63f00dd93db5890
kumailalidev <kumailalidev@gmail.com>
2023-11-17 16:22:02 +0500

    P02-02-Creating API Views.

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view()
+def product_list(
+    request,
+):  # with api_view  decorator applied, the request is instance of Request object comes with Django REST framework
+    return Response("OK")
+
+
+@api_view()
+def product_detail(request, id):
+    return Response(id)
