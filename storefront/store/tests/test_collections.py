81eb3c0a8e82c3fbe6e1451c84245868abf8faae
kumailalidev <kumailalidev@gmail.com>
2023-11-22 15:51:55 +0500

    P03-05-Authenticating the User.

--- storefront/store/tests/test_collections.py
+++ storefront/store/tests/test_collections.py
@@ -22,3 +22,13 @@ class TestCreateCollection:
 
         # Assert (checking behavior)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        """
+        If the authenticated user is not admin we should get 403 error.
+        """
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post("/store/collections/", {"title": "a"})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
