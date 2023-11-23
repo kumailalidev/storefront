c8c26c6c0e74b05d072bc46bcc7450ae7fc1b345
kumailalidev <kumailalidev@gmail.com>
2023-11-23 15:43:35 +0500

    [storefront project] Refactored celery.py.

--- storefront/storefront/celery.py
+++ storefront/storefront/celery.py
@@ -4,13 +4,6 @@ from celery import Celery
 # Setting Django settings as environment variable
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storefront.settings")
 
-# creating celery instance
 celery = Celery("storefront")
-
-# telling celery where it can find settings related to celery
-celery.config_from_object(
-    "django.conf:settings", namespace="CELERY"
-)  # :settings is an object inside django.conf module. namespace tells all the settings will start with CELERY
-
-# automatically discover all the tasks (inside tasks.py)
+celery.config_from_object("django.conf:settings", namespace="CELERY")
 celery.autodiscover_tasks()
