323ffeb6edfb6275ccc74af05e71b7f92f1016e2
kumailalidev <kumailalidev@gmail.com>
2023-11-23 16:19:32 +0500

    [store app] Refactored permissions.py.

--- storefront/store/permissions.py
+++ storefront/store/permissions.py
@@ -3,7 +3,6 @@ from rest_framework import permissions
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        # only allow GET, HEAD and OPTIONS methods to non authenticated user
         if request.method in permissions.SAFE_METHODS:
             return True
         return bool(request.user and request.user.is_staff)
